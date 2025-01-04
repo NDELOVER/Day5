64e8c10e5e959595d44dcc840830c9205be80a9b
Mehrnaz <mehrnaz.nemati2000@gmail.com>
2025-01-04 14:35:16 -0500

    retry all projects

---
+++ password Generator.py
@@ -0,0 +1,30 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+user_password = []
+i = 0
+a = 0
+b = 0
+while i < nr_letters:
+    user_password.append(random.choice(letters))
+    i+= 1
+while a < nr_symbols:
+    user_password.append(random.choice(symbols))
+    a += 1
+while b < nr_numbers:
+    user_password.append(random.choice(numbers))
+    b += 1
+
+print(user_password)
+random.shuffle(user_password)
+password = ''.join(user_password)
+print(password)
+
+
+
---
+++ range.py
@@ -0,0 +1,10 @@
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
+
+list_of_num = []
+for number in range(1,101):
+    list_of_num.append(number)
+print(list_of_num)
+print(sum(list_of_num))
